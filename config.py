0cfd05fec0f1fd3dd93f04281f409caece871489
Chiru63019 <hasenorc@gmail.com>
2025-03-22 00:53:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
-    API_ID = 22581733
-    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
-    ADMIN = '6530997270'.split(',')
+    BOT_TOKEN = "7641591924:AAHxXawteLZDb1WvSYbjF4KXqBFbTGI7jUc"
+    API_ID = 26468828
+    API_HASH = "4693513c08d1ac6af15f95b116c29478"
+    ADMIN = '7517045929'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://mewadonlinestudy:yYjxKiZGQag8DnWH@cpvod.ywpecvo.mongodb.net/?retryWrites=true&w=majority&appName=cpvodak"
     DB_NAME = "Crack_Jack"
-    TXT_LOG = -1002294532244
-    AUTH_LOG = -1002294532244
-    HIT_LOG = -1002294532244
-    DRM_DUMP = -1002294532244
-    CHANNEL = --1002487777733
-    CH_URL = "https://t.me/justfortestinggwak"
-    OWNER = "https://t.me/mrcraacker"
+    TXT_LOG = -1002692850343
+    AUTH_LOG = -1002692850343
+    HIT_LOG = -1002692850343
+    DRM_DUMP = -1002692850343
+    CHANNEL = -1002692850343
+    CH_URL = "https://t.me/skillwithchiru"
+    OWNER = "https://t.me/chiru52"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
     HOST = "https://api.masterapi.tech"
